62d2d5d679e52f3025933ebef339b833b7282928
Daniel Fontoura <ydnnl7@gmail.com>
2025-03-15 13:09:29 -0300

    Pequenos ajustes para reduzir o código

--- src/NivelBasico/Desafios/desafio2/desafio2.java
+++ src/NivelBasico/Desafios/desafio2/desafio2.java
@@ -8,9 +8,7 @@ public class desafio2 {
         Scanner entrada = new Scanner(System.in);
         int quantidadeNinjas = 0;
         String[] cadastroNinjas = new String[quantidadeNinjas];
-        String[] listaNinjas = new String[quantidadeNinjas];
-        int escolha = 0;
-        int sair = 0;
+        int escolha, sair = 0;
         do {
             System.out.println("\n===== Menu Ninja =====");
             System.out.println("1. Cadastrar Ninja");
@@ -19,31 +17,27 @@ public class desafio2 {
             System.out.print("Escolha uma opção: ");
             escolha = entrada.nextInt();
 
-            if (escolha == 1 && cadastroNinjas.length == 0) {
-                System.out.print("Quantos ninjas vocé deseja cadastrar? -> ");
-                quantidadeNinjas = entrada.nextInt();
-                entrada.nextLine();
-                cadastroNinjas = new String[quantidadeNinjas];
-                escolha = 1;
-
-            } else if (escolha == 2 && cadastroNinjas.length == 0) {
-                System.out.println("Nenhum ninja cadastrado. Por favor, cadastre um ninja primeiro.");
-            } else if (escolha == 2 && cadastroNinjas.length > 0) {
-                System.out.println(" ");
-                System.out.println("Lista de ninjas cadastrados: ");
-            }
-
             switch (escolha) {
                 case 1:
+                    System.out.print("Quantos ninjas vocé deseja cadastrar? -> ");
+                    quantidadeNinjas = entrada.nextInt();
+                    entrada.nextLine();
+                    cadastroNinjas = new String[quantidadeNinjas];
+
                     for (int i = 0; i < cadastroNinjas.length; i++) {
                         System.out.print("Digite o nome do " + (i + 1) + "º ninja: ");
                         cadastroNinjas[i] = entrada.nextLine();
                     }
-                    listaNinjas = cadastroNinjas.clone();
                     break;
                 case 2:
-                    for (int i = 0; i < listaNinjas.length; i++) {
-                        System.out.println("" + (i + 1) + " - " + listaNinjas[i]);
+                    if (cadastroNinjas.length > 0) {
+                        System.out.println(" ");
+                        System.out.println("Lista de ninjas cadastrados: ");
+                    } else if (escolha == 2 && cadastroNinjas.length == 0) {
+                        System.out.println("Nenhum ninja cadastrado. Por favor, cadastre um ninja primeiro.");
+                    }
+                    for (int i = 0; i < cadastroNinjas.length; i++) {
+                        System.out.println("" + (i + 1) + " - " + cadastroNinjas[i]);
                     }
                     break;
                 case 3:
