9b2d867149ac12210b09308642003f1207fe1169
Daniel Fontoura <ydnnl7@gmail.com>
2025-03-22 13:16:27 -0300

    Aprendizado em métodos para classes em Java

--- src/NivelIntermediario/Main.java
+++ src/NivelIntermediario/Main.java
@@ -14,6 +14,12 @@ public class Main {
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.idade = 18;
 
+        //Aplicando métodos ao objeto
+        Sasuke.SharinganAtivado();
+        System.out.println(Sasuke.EuSouUmNinja());
+
+        System.out.println("Você tem: " + Sasuke.idade + " anos, então falta " + Sasuke.anosParaSeTornarHokage(30) + " anos para você poder virar um hokage");
+
         // Criar Sakura Haruno
         Ninja Sakura = new Ninja();
         Sakura.aldeia = "Aldeia da folha";
